3f82185ffadcc1193dd73a6f3d9d285b0ee28303
garpil124 <kurniawansyahrifky122@gmail.com>
2025-11-17 09:05:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,34 +1,7 @@
-"""
-import os
-
-class Config:
-    startmsg = (
-        'The bot is up and running. These bots '
-        'can store messages in custom chats, '
-        'and users access them through the bot.'
-    )
-    forcemsg = (
-        'To view messages shared by bots. '
-        'Join first, then press the Try Again button.'
-    )
-
-    OWNER_ID = int(os.environ.get('OWNER_ID', ''))
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
-    DATABASE_ID = int(os.environ.get('DATABASE_ID', '0'))  # Berikan nilai default '0'
-    MONGO_URL = os.environ.get('MONGO_URL', '')
-
-    API_ID = 2040
-    API_HASH = 'b18441'
-    BOT_ID = BOT_TOKEN.split(':', 1)[0]
-
-Config = Config()
-"""
 from dotenv import load_dotenv
 import os
 
-# Memuat file .env dari direktori root
 load_dotenv()
-
 
 class Config:
     startmsg = (
@@ -41,14 +14,13 @@ class Config:
         'Join first, then press the Try Again button.'
     )
 
-    OWNER_ID = int(os.environ.get('OWNER_ID', ''))
+    OWNER_ID = int(os.environ.get('OWNER_ID', '0'))
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
-    DATABASE_ID = int(os.environ.get('DATABASE_ID', ''))
+    DATABASE_ID = int(os.environ.get('DATABASE_ID', '0'))
     MONGO_URL = os.environ.get('MONGO_URL', '')
 
-    API_ID = 
-    API_HASH = ''
+    API_ID = int(os.environ.get('API_ID', '0'))
+    API_HASH = os.environ.get('API_HASH', '')
     BOT_ID = BOT_TOKEN.split(':', 1)[0]
-
 
 Config = Config()
